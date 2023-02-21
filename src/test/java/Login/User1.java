c02189922759ebea89d21bf9c058e0d7bd82e10a
KiruthigaDaran <kiruthigadaran@gmail.com>
2023-02-21 09:00:12 +0530

    Initial commit

---
+++ src/test/java/Login/User1.java
@@ -0,0 +1,21 @@
+package Login;
+
+public class User1 {
+	
+	public void EnterUsername() {
+		System.out.println("Username");
+		
+	}
+	public void EnterPassword() {
+		System.out.println("Password");
+		
+	}
+
+	public static void main(String[] args) {
+    
+		User1 Obj=new User1();
+		Obj.EnterUsername();
+		Obj.EnterPassword();
+	}
+
+}
